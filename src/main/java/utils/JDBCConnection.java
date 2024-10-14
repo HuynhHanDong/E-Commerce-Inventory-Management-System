d0e75918f72e5df00697a7cf853c5bf596f06cf1
NT4105 <tunguyen04105@gmail.com>
2024-10-14 22:45:06 +0700

    fix

---
+++ src/main/java/utils/JDBCConnection.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JDBCConnection {
+    private String hostname = "localhost";
+    private String port = "1433";
+    private String databaseName = "ECommerceDB";
+    private String username = "sa";
+    private String password = "123123123";
+
+    public void connect() {
+        String url = "jdbc://sqlserver://" + hostname + ":" + port + "/" + databaseName;
+
+        try {
+            try {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
