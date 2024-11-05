3b50355f611ae92d2b9871e27e378da74c2a1046
NT4105 <tunguyen04105@gmail.com>
2024-11-05 22:36:00 +0700

    fix menu (delete char t)

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -10,7 +10,7 @@ package view;
  * @author Huynh Han Dong
  */
 public class Menu {
-    public void loginMenu(){
+    public void loginMenu() {
         System.out.println("+--------------- Login ----------------+");
         System.out.println("| Welcome! you are?                    +");
         System.out.println("| 1. Store owner                       +");
@@ -18,8 +18,8 @@ public class Menu {
         System.out.println("| 0. Exit                              +");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void customerMenu(){
+
+    public void customerMenu() {
         System.out.println("+----------- Customer Menu ------------+");
         System.out.println("| 1. View all Products                 |");
         System.out.println("| 2. Search product                    |");
@@ -29,8 +29,8 @@ public class Menu {
         System.out.println("| 0. Logout                            |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void ownerMenu(){
+
+    public void ownerMenu() {
         System.out.println("+------------- Owner Menu -------------+");
         System.out.println("| 1. Manage products                   |");
         System.out.println("| 2. Manage categories                 |");
@@ -39,8 +39,8 @@ public class Menu {
         System.out.println("| 0. Logout                            |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void productMenu(){
+
+    public void productMenu() {
         System.out.println("+------------ Product Menu ------------+");
         System.out.println("| 1. Add product                       |");
         System.out.println("| 2. Update product details            |");
@@ -50,8 +50,8 @@ public class Menu {
         System.out.println("| 0. Go back                           |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void categorytMenu(){
+
+    public void categoryMenu() {
         System.out.println("+ ----------- Category Menu -----------+");
         System.out.println("| 1. Add category                      |");
         System.out.println("| 2. Update category                   |");
@@ -61,18 +61,18 @@ public class Menu {
         System.out.println("| 0. Go back                           |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void inventorytMenu(){
+
+    public void inventoryMenu() {
         System.out.println("+----------- Inventory Menu -----------+");
         System.out.println("| 1. Add inventory item                |");
         System.out.println("| 2. Update inventory item             |");
-        System.out.println("| 1. View stock levels by product      |");
-        System.out.println("| 2. View stock levels of all products |");
-        System.out.println("| 0. Go back                           |"); 
+        System.out.println("| 3. View stock levels by product      |");
+        System.out.println("| 4. View stock levels of all products |");
+        System.out.println("| 0. Go back                           |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void reporttMenu(){
+
+    public void reportMenu() {
         System.out.println("+------------ Report Menu -------------+");
         System.out.println("| 1. Generate sales report             |");
         System.out.println("| 2. View sales reports                |");
@@ -81,8 +81,8 @@ public class Menu {
         System.out.println("| 0. Go back                           |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void ordertMenu(){
+
+    public void orderMenu() {
         System.out.println("+------------- Order Menu -------------+");
         System.out.println("| 1. Add to cart                       |");
         System.out.println("| 2. Change item amount                |");
@@ -92,15 +92,15 @@ public class Menu {
         System.out.println("| 0. Go back                           |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void confirmOrderMenu(){
+
+    public void confirmOrderMenu() {
         System.out.println("+----------- Confirm Order ------------+");
         System.out.println("| 1. YES                               |");
         System.out.println("| 0. NO                                |");
         System.out.println("+--------------------------------------+");
     }
-    
-    public void orderHistoryMenu(){
+
+    public void orderHistoryMenu() {
         System.out.println("+----------- Order History ------------+");
         System.out.println("| 1. View all                          |");
         System.out.println("| 2. View status                       |");
@@ -109,7 +109,7 @@ public class Menu {
         System.out.println("+--------------------------------------+");
     }
 
-    public void orderHistorySubMenu(){
+    public void orderHistorySubMenu() {
         System.out.println("+----------- Order History ------------+");
         System.out.println("| 1. Pending                           |");
         System.out.println("| 2. In progress                       |");
